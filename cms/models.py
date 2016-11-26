47fa617fac58a08ce8bae564bbeec5e2d07e9ad1
Scot Hacker <shacker@birdhouse.org>
2016-11-26 09:06:48 -0800

    Add section container model

--- cms/models.py
+++ cms/models.py
@@ -26,3 +26,11 @@ class ContentPage(Page):
 
     def __str__(self):
         return "{0} - {1}".format(self.title, self.owner)
+
+
+class SectionPage(Page):
+
+    pass
+
+    def __str__(self):
+        return self.title
