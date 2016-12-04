b18e2f4c428a927ab96e83e5a25ec00345c29546
Scot Hacker <shacker@birdhouse.org>
2016-12-04 15:19:04 -0800

    Contact rep MVP

--- beheard/forms.py
+++ beheard/forms.py
@@ -5,7 +5,19 @@ class LookupForm(forms.Form):
 
     zip = forms.CharField(label='Zip Code', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
+    def clean_zip(self):
+        data = self.cleaned_data['zip']
+        if not data.isdigit():
+            raise forms.ValidationError("Zip code must be all digits")
+        if not len(data) == 5:
+            raise forms.ValidationError("Zip code must be five digits")
+        return data
+
 
 class BeHeardForm(forms.Form):
 
-    msg = forms.CharField(label='Message Body', required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    msg = forms.CharField(
+        label='Message Body',
+        required=True,
+        widget=forms.Textarea(attrs={'class': 'form-control'}),
+        initial="foo")
--- beheard/views.py
+++ beheard/views.py
@@ -1,6 +1,12 @@
+import json
+import requests
+
+from django.core.mail import EmailMessage
+from django.core.cache import cache
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.template.loader import render_to_string
 
 from beheard.forms import LookupForm, BeHeardForm
 
@@ -33,10 +39,40 @@ def edit_and_send(request, zip):
     Look up senators and rep by zip, provide form to send email to them.
     """
 
+    cache_name = "zipresults_{z}".format(z=zip)
+    results = cache.get(cache_name)
+    if not results:
+        url = 'https://congress.api.sunlightfoundation.com/legislators/locate?zip={zip}'.format(zip=zip)
+        req = requests.request('GET', url)
+        results = json.loads(req.text)['results']
+        cache.set(cache_name, results, 60 * 10)
+
     if request.method == "POST":
 
         form = BeHeardForm(request.POST)
         if form.is_valid():
+            personal_message = form.cleaned_data['msg']
+
+            ids = request.POST.getlist('rep_ids')
+            if ids:
+                for i in ids:
+                    # Get email for rep with matching ID key
+                    for rep in results:
+                        if rep.get('govtrack_id') == i:
+                            subject = 'I support pro-climate policies like the Paris Agreement and the Clean Power Plan'
+                            message = render_to_string(
+                                "beheard/email/beheard.txt",
+                                {'rep': rep, 'personal_message': personal_message})
+                            msg = EmailMessage(
+                                subject,
+                                message,
+                                from_email='Cross the Aisle for Climate <crossforclimate@gmail.com>',
+                                to=[rep.get('oc_email'), ],
+                                bcc=['shacker@birdhouse.org', 'rsalvadorreyes@mac.com', ])
+                            msg.send(fail_silently=False)
+
+                    print("sending to ", i)
+
             return redirect(reverse('beheard_thanks'))
         else:
             messages.error(request, "There were errors in the form.")
@@ -47,5 +83,14 @@ def edit_and_send(request, zip):
     return render(
         request,
         'beheard/beheard.html',
+        locals(),
+        )
+
+
+def beheard_thanks(request):
+
+    return render(
+        request,
+        'beheard/beheard_thanks.html',
         locals(),
         )
